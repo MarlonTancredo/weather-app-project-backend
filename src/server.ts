3dd1b22ce7d6258d0645a5188f5ca4eb6a15e9a7
unknown <marlon_tancredo@hotmail.com>
2023-09-21 18:26:44 +0100

    feat: making a call to geocoding api

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+//Testing geocode api
+
+async function fetchGeoLocation() {
+  const response = await fetch(
+    "https://geocoding-api.open-meteo.com/v1/search?name=London&count=10https://geocoding-api.open-meteo.com/v1/search?name=London&count=1"
+  );
+
+  const data = await response.json();
+  console.log(data);
+}
+
+fetchGeoLocation();
