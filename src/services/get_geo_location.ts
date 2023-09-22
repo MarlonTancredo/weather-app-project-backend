10904656d6790e5cc9d196e7efbacd31814fc83e
unknown <marlon_tancredo@hotmail.com>
2023-09-22 13:37:58 +0100

    feat: implementing services

---
+++ src/services/get_geo_location.ts
@@ -0,0 +1,10 @@
+export const getGeoLocation = (cityName = "London") => {
+  return {
+    cityName,
+    latitude: "52.52437",
+    longitude: "13.41053",
+  };
+};
+
+//We will return to the user an abject with all  weather information related to city passed in the end point.
+// I am currently creating the logic.
