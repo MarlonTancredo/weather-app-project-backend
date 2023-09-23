b3bf22b340b9f46577be04e41704864405a09e76
unknown <marlon_tancredo@hotmail.com>
2023-09-23 19:48:45 +0100

    refact: refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
 import { app } from "./app";
-import { getWeatherForecast } from "./services/get_weather_forecast";
 
 export const server = app.listen(3000, async () => {
-	console.log("Server running on port 3000");
-	const f = await getWeatherForecast(56, 23.9);
-	//console.log(f);
+  console.log("Server running on port 3000");
 });
