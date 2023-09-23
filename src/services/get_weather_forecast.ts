0ca01b5dd370101aafbf092d2985bf23af8b7173
unknown <marlon_tancredo@hotmail.com>
2023-09-24 00:18:04 +0100

    feat: implementing get_weather_forecast

--- src/services/get_weather_forecast.ts
+++ src/services/get_weather_forecast.ts
@@ -1,33 +1,7 @@
-import { WeatherDetails } from "../models/types";
-import { LocationForecast } from "../models/types";
+import { LocationForecast, WeatherDetails } from "../models/types";
+import { fetchWeatherFromAPI } from "../repository/fetch_weather_from_api";
 
-export const getWeatherForecast = async (
-  latitude = 51.50853,
-  longitude = -0.12574
-): Promise<WeatherDetails | LocationForecast> => {
-  const response = await fetch(
-    `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${latitude}&lon=${longitude}`
-  );
-  const data = await response.json();
-
-  const { updated_at } = data.properties.meta;
-
-  const { air_temperature, wind_from_direction, wind_speed } =
-    data.properties.timeseries[0].data.instant.details;
-
-  const { air_temperature_max, air_temperature_min } =
-    data.properties.timeseries[0].data.next_6_hours.details;
-
-  const { symbol_code } =
-    data.properties.timeseries[0].data.next_12_hours.summary;
-
-  return {
-    updated_at: updated_at,
-    current_air_temperature: air_temperature,
-    wind_from_direction: wind_from_direction,
-    wind_speed: wind_speed,
-    high_air_temperature: air_temperature_max,
-    low_air_temperature: air_temperature_min,
-    symbol_code: symbol_code,
-  };
-};
+export async function getWeatherForecast(latitude: number, longitude: number) {
+  const f = await fetchWeatherFromAPI(latitude, longitude);
+  console.log("getWeatherForecast works");
+}
