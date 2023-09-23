6e27ba3cef17fa66ec8583e0c683d496af956c64
unknown <marlon_tancredo@hotmail.com>
2023-09-24 00:10:23 +0100

    feat: adding updated_at value to getWeatherForecast

--- src/services/get_weather_forecast.ts
+++ src/services/get_weather_forecast.ts
@@ -1,13 +1,16 @@
 import { WeatherDetails } from "../models/types";
+import { LocationForecast } from "../models/types";
 
-export const getWeatherService = async (
+export const getWeatherForecast = async (
   latitude = 51.50853,
   longitude = -0.12574
-): Promise<WeatherDetails> => {
+): Promise<WeatherDetails | LocationForecast> => {
   const response = await fetch(
     `https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=${latitude}&lon=${longitude}`
   );
   const data = await response.json();
+
+  const { updated_at } = data.properties.meta;
 
   const { air_temperature, wind_from_direction, wind_speed } =
     data.properties.timeseries[0].data.instant.details;
@@ -19,6 +22,7 @@ export const getWeatherService = async (
     data.properties.timeseries[0].data.next_12_hours.summary;
 
   return {
+    updated_at: updated_at,
     current_air_temperature: air_temperature,
     wind_from_direction: wind_from_direction,
     wind_speed: wind_speed,
