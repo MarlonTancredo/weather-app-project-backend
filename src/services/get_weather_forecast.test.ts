cbbc92f9281160c99d4247dd5a03bd2a13e82268
unknown <marlon_tancredo@hotmail.com>
2023-09-23 19:52:46 +0100

    refact: refactoring

--- src/services/get_weather_forecast.test.ts
+++ src/services/get_weather_forecast.test.ts
@@ -1,3 +1,3 @@
-import getWeatherForecast from "./get_weather_forecast";
-import mockWeatherData from "../models/weather_mock_data";
-describe("return mock data for getWeatherForecast", () => {});
+// import getWeatherForecast from "./get_weather_forecast";
+// import mockWeatherData from "../models/weather_mock_data";
+// describe("return mock data for getWeatherForecast", () => {});
