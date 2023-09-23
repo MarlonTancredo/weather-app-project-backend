4aa07182e36a193d0340037fd6f1536563d2e166
unknown <marlon_tancredo@hotmail.com>
2023-09-23 16:36:19 +0100

    refactor: refactoring geo_location_controller

--- src/controllers/geo_location_controller.ts
+++ src/controllers/geo_location_controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
-import * as geoLocationService from "../services/get_geo_location";
+import { getGeoLocationService } from "../services/get_geo_location";
 
 export const getGeoLocation = async (
   req: Request<object, object, object, { cityName: string | undefined }>,
   res: Response
 ) => {
   const cityName = req.query.cityName;
-  const city = geoLocationService.getGeoLocation(cityName);
-  res.json(city).status(200);
+  const cityGeoLocation = await getGeoLocationService(cityName);
+  res.json(cityGeoLocation).status(200);
 };
