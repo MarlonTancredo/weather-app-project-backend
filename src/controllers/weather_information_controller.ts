0d28c876115fe13ef0552ddc081d40d1dcb55790
unknown <marlon_tancredo@hotmail.com>
2023-09-23 18:07:34 +0100

    refactor: refactoring

---
+++ src/controllers/weather_information_controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { getGeoLocationService } from "../services/get_geo_location";
+import { getWeatherService } from "../services/get_weather_forecast";
+
+export const getWeatherInformation = async (
+  req: Request<object, object, object, { cityName: string | undefined }>,
+  res: Response
+) => {
+  const cityName = req.query.cityName;
+
+  const geoLocation = await getGeoLocationService(cityName);
+  const { latitude, longitude } = geoLocation;
+
+  const weatherInformation = await getWeatherService(latitude, longitude);
+
+  res.json({ geoLocation, weatherInformation }).status(200);
+};
