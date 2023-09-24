4f09da980287ea17323c335d7fe62d9f890f096f
zpran <zanna@prane.org>
2023-09-24 13:48:11 +0100

    fix: change weather function to zanna;s

--- src/controllers/weather_information_controller.ts
+++ src/controllers/weather_information_controller.ts
@@ -1,21 +1,25 @@
 import { Request, Response } from "express";
 import { getGeoLocationService } from "../services/get_geo_location";
 import { getWeatherService } from "../services/get_weather_service";
+import { getWeatherForecast } from "../services/get_weather_forecast";
 
 export const getWeatherInformation = async (
-  req: Request<object, object, object, { cityName: string | undefined }>,
-  res: Response
+	req: Request<object, object, object, { cityName: string | undefined }>,
+	res: Response
 ) => {
-  try {
-    const cityName = req.query.cityName;
+	try {
+		const cityName = req.query.cityName;
 
-    const geoLocation = await getGeoLocationService(cityName);
-    const { latitude, longitude } = geoLocation;
+		const geoLocation = await getGeoLocationService(cityName);
+		const { latitude, longitude } = geoLocation;
 
-    const weatherInformation = await getWeatherService(latitude, longitude);
+		const weatherInformation = await getWeatherForecast(
+			latitude,
+			longitude
+		);
 
-    res.json({ geoLocation, weatherInformation }).status(200);
-  } catch ({ message }: any) {
-    res.json(message);
-  }
+		res.json({ geoLocation, weatherInformation }).status(200);
+	} catch ({ message }: any) {
+		res.json(message);
+	}
 };
