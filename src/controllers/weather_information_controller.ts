be690aba1ed72e7654805395c9f450888b99cbb5
zpran <zanna@prane.org>
2023-09-26 15:08:20 +0300

    feat: implemented localTime in controller

--- src/controllers/weather_information_controller.ts
+++ src/controllers/weather_information_controller.ts
@@ -1,20 +1,27 @@
 import { Request, Response } from "express";
 import { getGeoLocationService } from "../services/get_geo_location";
 import { getWeatherForecast } from "../services/get_weather_forecast";
+import { fetchLocalTimeFromAPI } from "../repository/fetch_local_time_from_api";
 
 export const getWeatherInformation = async (
-  req: Request<object, object, object, { cityName: string | undefined }>,
-  res: Response
+	req: Request<object, object, object, { cityName: string | undefined }>,
+	res: Response
 ) => {
-  try {
-    const cityName = req.query.cityName;
+	try {
+		const cityName = req.query.cityName;
 
-    const geoLocation = await getGeoLocationService(cityName);
-    const { latitude, longitude } = geoLocation;
+		const geoLocation = await getGeoLocationService(cityName);
+		const { latitude, longitude } = geoLocation;
 
-    const weatherInformation = await getWeatherForecast(latitude, longitude);
-    res.json({ geoLocation, weatherInformation }).status(200);
-  } catch ({ message }: any) {
-    res.json(message);
-  }
+		const localTime = await fetchLocalTimeFromAPI(geoLocation.timezone);
+		console.log(localTime);
+
+		const weatherInformation = await getWeatherForecast(
+			latitude,
+			longitude
+		);
+		res.json({ geoLocation, localTime, weatherInformation }).status(200);
+	} catch ({ message }: any) {
+		res.json(message);
+	}
 };
