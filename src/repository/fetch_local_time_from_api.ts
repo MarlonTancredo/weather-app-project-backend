c63adcf31410bd2a36c88f9a45f4bcf1b8477422
zpran <zanna@prane.org>
2023-09-26 15:01:35 +0300

    feat: implemented fetchLocalTimeFromAPI()

---
+++ src/repository/fetch_local_time_from_api.ts
@@ -0,0 +1,15 @@
+export const fetchLocalTimeFromAPI = async (timezone = "Europe/London") => {
+	const requestString = `https://timeapi.io/api/Time/current/zone?timeZone=${timezone}`;
+	try {
+		const response = await fetch(requestString);
+		if (response.ok) {
+			console.log("Fetched: " + requestString);
+			return await response.json();
+		} else {
+			console.log(requestString);
+			console.log(response.status, response.statusText);
+		}
+	} catch (error) {
+		return Promise.reject(error);
+	}
+};
